35d467aac51aba6a9ff850455aaa106fef2604b6
edh <edh@google.com>
2017-10-05 22:59:50 -0700

    created Firestore rules and indexes

--- src/app/patron/patron.component.ts
+++ src/app/patron/patron.component.ts
@@ -48,31 +48,14 @@ export class PatronComponent implements OnInit {
         this.isPouring = false;
         this.isLoaded = true;
 
-        // this.render();
+        this.render();
         // this.init();
     }
 
-    hasClass (element, cls) {
-        return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
-    }
-
-    addClass (element, cls) {
-        if (!this.hasClass(element, cls)) {
-            element.className += ' ' + cls;
-        }
-    }
-
-    removeClass (element, cls) {
-        if (this.hasClass(element, cls)) {
-            const reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
-            element.className = element.className.replace(reg, ' ');
-        }
-    }
-
-    liquidAnimation (el, speed) {
+    liquidAnimation(el, speed) {
         const liquidBackgroundPositionArr = getComputedStyle(el)
-                                                .getPropertyValue('background-position')
-                                                .split(' ');
+            .getPropertyValue('background-position')
+            .split(' ');
         const liquidBackgroudPosition = parseFloat(liquidBackgroundPositionArr[1]);
         if (liquidBackgroudPosition > 0) {
             el.style.backgroundPosition = '50% ' + ((liquidBackgroudPosition - speed) + '%');
@@ -86,16 +69,6 @@ export class PatronComponent implements OnInit {
     render() {
         if (this.isPouring) {
             this.liquidAnimation(this.pouringLiquid, 0.5);
-            this.addClass(this.pourButton, 'clicked');
-            this.addClass(this.pouringLiquid, 'pouring');
-            this.beerTap.setAttribute('class', 'clicked');
-            this.beerTapShadow.setAttribute('class', 'clicked');
-
-        } else {
-            this.removeClass(this.pourButton, 'clicked');
-            this.removeClass(this.pouringLiquid, 'pouring');
-            this.beerTap.setAttribute('class', '');
-            this.beerTapShadow.setAttribute('class', '');
         }
         requestAnimationFrame(this.render);
     }
@@ -103,19 +76,4 @@ export class PatronComponent implements OnInit {
     toggleIsPouring() {
         this.isPouring = !this.isPouring;
     }
-
-    init() {
-        const that = this;
-        this.pourButton.addEventListener('mousedown', function (e) {
-            that.isPouring = true;
-        });
-        document.documentElement.addEventListener('mouseup', function (e) {
-            that.isPouring = false;
-        });
-        window.onload = function () {
-            that.addClass(document.getElementById('instructions-container'), 'loaded');
-        };
-    }
-
-
 }
